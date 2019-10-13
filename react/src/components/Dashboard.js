1c30abc3fc41f41a3d82610d9fa33c4ad037c61b
Hazrat Ali <iloveyii@yahoo.com>
2019-10-13 23:43:33 +0200

    Added Pie

--- react/src/components/Dashboard.js
+++ react/src/components/Dashboard.js
@@ -4,36 +4,39 @@ import {withRouter} from "react-router-dom";
 
 import {apiServer} from '../common/constants';
 import Sidebar from './Sidebar';
+import PieDemo from './PieDemo';
 
-import { BarChart, Bar,ResponsiveContainer, LineChart, Line,  ReferenceLine,
-    ReferenceDot, CartesianGrid,XAxis, Cell, YAxis, Tooltip, Legend, Brush, ErrorBar, AreaChart, Area,
-    Label, LabelList } from 'recharts';
+import {
+    BarChart, Bar, ResponsiveContainer, LineChart, Line, ReferenceLine,
+    ReferenceDot, CartesianGrid, XAxis, Cell, YAxis, Tooltip, Legend, Brush, ErrorBar, AreaChart, Area,
+    Label, LabelList
+} from 'recharts';
 
 
 const endPoint = '/v2/calculator/api/?zone=';
 const server = apiServer + endPoint;
 
 const data2 = [
-    { name: 'Page A', uv: 300, pv: 2600, amt: 3400 },
-    { name: 'Page B', uv: 400, pv: 4367, amt: 6400 },
-    { name: 'Page C', uv: 300, pv: 1398, amt: 2400 },
-    { name: 'Page D', uv: 200, pv: 9800, amt: 2400 },
-    { name: 'Page E', uv: 278, pv: 3908, amt: 2400 },
-    { name: 'Page F', uv: 189, pv: 4800, amt: 2400 },
-    { name: 'Page G', uv: 189, pv: 4800, amt: 2400 },
+    {name: 'Page A', uv: 300, pv: 2600, amt: 3400},
+    {name: 'Page B', uv: 400, pv: 4367, amt: 6400},
+    {name: 'Page C', uv: 300, pv: 1398, amt: 2400},
+    {name: 'Page D', uv: 200, pv: 9800, amt: 2400},
+    {name: 'Page E', uv: 278, pv: 3908, amt: 2400},
+    {name: 'Page F', uv: 189, pv: 4800, amt: 2400},
+    {name: 'Page G', uv: 189, pv: 4800, amt: 2400},
 ];
 
 const data = [
-    { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
-    { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
-    { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
-    { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
-    { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
-    { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
-    { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
-    { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
-    { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
-    { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
+    {name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20]},
+    {name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40]},
+    {name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40},
+    {name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20},
+    {name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28},
+    {name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20]},
+    {name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40]},
+    {name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28},
+    {name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28},
+    {name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60]},
 ];
 
 
@@ -50,52 +53,51 @@ class Dashboard extends React.Component {
                 <div className="dashboard--center" onClick={this.handleCenterClick}>
                     <h1>Dashboard</h1>
                     <div style={{padding: '40px', backgroundColor: 'white', borderRadius: '3px'}}>
-                        <div
-                            style={{
-                                width: '400px',
-                                height: '300px'
-                            }}
-                        >
 
-                            <LineChart
-                                width={400}
-                                height={400}
-                                data={data}
-                                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-                            >
-                                <XAxis dataKey="name" />
-                                <Tooltip />
-                                <CartesianGrid stroke="#f5f5f5" />
-                                <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-                                <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
-                            </LineChart>
-
-
-                            <BarChart width={400} height={400} data={data} onClick={this.handlePvBarClick}>
-                                <XAxis dataKey="name" />
-                                <YAxis yAxisId="a" />
-                                <YAxis yAxisId="b" orientation="right" />
-                                <Legend />
-                                <Tooltip />
-                                <CartesianGrid vertical={false} />
-                                <Bar yAxisId="a" dataKey="uv" onAnimationStart={this.handleBarAnimationStart} onAnimationEnd={this.handleBarAnimationEnd}>
-                                    <LabelList fill="#000" angle={-45} />
-                                    {
-                                        data.map((entry, index) => (
-                                            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                                        ))
-                                    }
-                                </Bar>
-                                <Bar yAxisId="b" dataKey="pv" label>
-                                    {
-                                        data.map((entry, index) => (
-                                            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                                        ))
-                                    }
-                                </Bar>
-                            </BarChart>
-
-
+                        <div className="row">
+                            <div className="col-1-of-1">
+                                <PieDemo/>
+                            </div>
+                            <div className="col-1-of-1">
+                                <LineChart
+                                    width={400}
+                                    height={400}
+                                    data={data}
+                                    margin={{top: 5, right: 20, left: 10, bottom: 5}}
+                                >
+                                    <XAxis dataKey="name"/>
+                                    <Tooltip/>
+                                    <CartesianGrid stroke="#f5f5f5"/>
+                                    <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0}/>
+                                    <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1}/>
+                                </LineChart>
+                            </div>
+                            <div className="col-1-of-1">
+                                <BarChart width={400} height={400} data={data} onClick={this.handlePvBarClick}>
+                                    <XAxis dataKey="name"/>
+                                    <YAxis yAxisId="a"/>
+                                    <YAxis yAxisId="b" orientation="right"/>
+                                    <Legend/>
+                                    <Tooltip/>
+                                    <CartesianGrid vertical={false}/>
+                                    <Bar yAxisId="a" dataKey="uv" onAnimationStart={this.handleBarAnimationStart}
+                                         onAnimationEnd={this.handleBarAnimationEnd}>
+                                        <LabelList fill="#000" angle={-45}/>
+                                        {
+                                            data.map((entry, index) => (
+                                                <Cell key={`cell-${index}`} fill={colors[index % 20]}/>
+                                            ))
+                                        }
+                                    </Bar>
+                                    <Bar yAxisId="b" dataKey="pv" label>
+                                        {
+                                            data.map((entry, index) => (
+                                                <Cell key={`cell-${index}`} fill={colors[index % 20]}/>
+                                            ))
+                                        }
+                                    </Bar>
+                                </BarChart>
+                            </div>
                         </div>
                     </div>
 
