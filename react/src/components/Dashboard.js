8e54e97f423c191cb242882226797aaf915605be
Hazrat Ali <iloveyii@yahoo.com>
2019-10-14 00:22:54 +0200

    Added simple squares

--- react/src/components/Dashboard.js
+++ react/src/components/Dashboard.js
@@ -31,8 +31,22 @@ class Dashboard extends React.Component {
                 <Sidebar/>
                 <div className="dashboard--center" onClick={this.handleCenterClick}>
                     <h1>Dashboard</h1>
-                    <div style={{padding: '40px', backgroundColor: 'white', borderRadius: '3px'}}>
+                    <div style={{padding: '40px', backgroundColor: 'lavenderblush', borderRadius: '3px'}}>
 
+                        <div className="row">
+                            <div className="col-1-of-4">
+                                <div style={{width: '100%', height: '300px', backgroundColor: 'red', borderRadius: '3px'}}></div>
+                            </div>
+                            <div className="col-1-of-4">
+                                <div style={{width: '100%', height: '300px', backgroundColor: 'orange', borderRadius: '3px'}}></div>
+                            </div>
+                            <div className="col-1-of-4">
+                                <div style={{width: '100%', height: '300px', backgroundColor: 'green', borderRadius: '3px'}}></div>
+                            </div>
+                            <div className="col-1-of-4">
+                                <div style={{width: '100%', height: '300px', backgroundColor: 'blue', borderRadius: '3px'}}></div>
+                            </div>
+                        </div>
                         <div className="row">
                             <div className="col-1-of-2">
                                 <Chart
@@ -134,6 +148,53 @@ class Dashboard extends React.Component {
                                         colorAxis: {colors: ['yellow', 'red']},
                                     }}
                                     rootProps={{'data-testid': '2'}}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-1-of-1">
+                                <Chart
+                                    width="100%"
+                                    height={'500px'}
+                                    chartType="Histogram"
+                                    loader={<div>Loading Chart</div>}
+                                    data={[
+                                        ['Dinosaur', 'Length'],
+                                        ['Acrocanthosaurus (top-spined lizard)', 12.2],
+                                        ['Albertosaurus (Alberta lizard)', 9.1],
+                                        ['Allosaurus (other lizard)', 12.2],
+                                        ['Apatosaurus (deceptive lizard)', 22.9],
+                                        ['Archaeopteryx (ancient wing)', 0.9],
+                                        ['Argentinosaurus (Argentina lizard)', 36.6],
+                                        ['Baryonyx (heavy claws)', 9.1],
+                                        ['Brachiosaurus (arm lizard)', 30.5],
+                                        ['Ceratosaurus (horned lizard)', 6.1],
+                                        ['Coelophysis (hollow form)', 2.7],
+                                        ['Compsognathus (elegant jaw)', 0.9],
+                                        ['Deinonychus (terrible claw)', 2.7],
+                                        ['Diplodocus (double beam)', 27.1],
+                                        ['Dromicelomimus (emu mimic)', 3.4],
+                                        ['Gallimimus (fowl mimic)', 5.5],
+                                        ['Mamenchisaurus (Mamenchi lizard)', 21.0],
+                                        ['Megalosaurus (big lizard)', 7.9],
+                                        ['Microvenator (small hunter)', 1.2],
+                                        ['Ornithomimus (bird mimic)', 4.6],
+                                        ['Oviraptor (egg robber)', 1.5],
+                                        ['Plateosaurus (flat lizard)', 7.9],
+                                        ['Sauronithoides (narrow-clawed lizard)', 2.0],
+                                        ['Seismosaurus (tremor lizard)', 45.7],
+                                        ['Spinosaurus (spiny lizard)', 12.2],
+                                        ['Supersaurus (super lizard)', 30.5],
+                                        ['Tyrannosaurus (tyrant lizard)', 15.2],
+                                        ['Ultrasaurus (ultra lizard)', 30.5],
+                                        ['Velociraptor (swift robber)', 1.8],
+                                    ]}
+                                    options={{
+                                        title: 'Lengths of dinosaurs, in meters',
+                                        legend: { position: 'none' },
+                                    }}
+                                    rootProps={{ 'data-testid': '1' }}
                                 />
                             </div>
                         </div>
