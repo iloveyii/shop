e9bec99357b1ade0f2659c57a09bfaa525c14311
Hazrat Ali <iloveyii@yahoo.com>
2019-10-14 00:53:59 +0200

    Added geo chart

--- react/src/components/Dashboard.js
+++ react/src/components/Dashboard.js
@@ -47,6 +47,29 @@ class Dashboard extends React.Component {
                                 <div style={{width: '100%', height: '300px', backgroundColor: 'blue', borderRadius: '3px'}}></div>
                             </div>
                         </div>
+
+                        <div className="row">
+                            <div className="col-1-of-1">
+                                <Chart
+                                    width={'100%'}
+                                    height={'700px'}
+                                    chartType="GeoChart"
+                                    data={[
+                                        ['Country', 'Popularity'],
+                                        ['Germany', 200],
+                                        ['United States', 300],
+                                        ['Brazil', 400],
+                                        ['Canada', 500],
+                                        ['France', 600],
+                                        ['RU', 700],
+                                    ]}
+                                    // Note: you will need to get a mapsApiKey for your project.
+                                    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+                                    mapsApiKey="YOUR_KEY_HERE"
+                                    rootProps={{ 'data-testid': '1' }}
+                                />
+                            </div>
+                        </div>
                         <div className="row">
                             <div className="col-1-of-2">
                                 <Chart
